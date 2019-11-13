b4015706c795090c1d7b1de9f0952f52c217d977
Atom <atom@filippi.com>
2019-11-13 14:52:50 +0000

    finished parsing

--- mccomp.h
+++ mccomp.h
@@ -102,6 +102,8 @@ extern int lineNo, columnNo;
 extern TOKEN CurTok;
 extern std::deque <TOKEN> tok_buffer;
 
+extern LogError(std::string )
+
 extern void putBackToken(TOKEN tok);
 
 TOKEN getNextToken();
